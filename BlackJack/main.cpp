508965e9ca04f129e9bb7d6f896ad2701064811f
Roberto Mtz <roberto@robertomtz.com>
2015-03-04 07:54:09 -0600

    Dibuja algo en la pantalla.

--- BlackJack/main.cpp
+++ BlackJack/main.cpp
@@ -1,15 +1,58 @@
-//
-//  main.cpp
-//  BlackJack
-//
-//  Created by Roberto Mtz on 3/4/15.
-//  Copyright (c) 2015 ITESM. All rights reserved.
-//
+#ifdef __APPLE__
+#include <GLUT/glut.h>
+#else
+#include <GL/glut.h>
+#endif
 
-#include <iostream>
+#include <stdlib.h>
+#include <stdio.h>
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+void init()
+{
+    // Para que las paredes se vean sólidas (no transparentes)
+    glEnable(GL_DEPTH_TEST);
+    
+    glEnable(GL_BLEND);
+    
+    
+}
+
+void dibuja()
+{
+    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+    
+    glPushMatrix() ;
+    glTranslated(0, 0, 0);
+    glutSolidCube(1);
+    glPopMatrix();
+    
+    glutSwapBuffers();
+}
+
+void reshape(int ancho, int alto)
+{
+    // Ventana
+    glViewport(0, 0, ancho, alto);
+    // Sistema de coordenadas
+    glMatrixMode(GL_PROJECTION);
+    glLoadIdentity();
+    glOrtho(-2, 2, -2, 2,1,12 ); //izq, der, abajo, arriba, cerca, lejos
+    glMatrixMode(GL_MODELVIEW);
+    glLoadIdentity();
+    gluLookAt(0, 0, 10, 0, 0, 0, 0, 1, 0);
+}
+
+int main(int argc, char *argv[])
+{
+    glutInit(&argc, argv);
+    glutInitWindowSize(640,480);
+    glutInitWindowPosition(10,10);
+    glutInitDisplayMode(GLUT_RGBA | GLUT_DEPTH| GLUT_DOUBLE );
+    glutCreateWindow("Blackjack A01190757");
+    init();
+    glutDisplayFunc(dibuja);
+    glutReshapeFunc(reshape);
+    glutMainLoop();
     return 0;
 }
+
