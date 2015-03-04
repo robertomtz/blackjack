d88add776bd1e96151e24e25aff2b979298c8251
Roberto Mtz <roberto@robertomtz.com>
2015-03-04 08:32:11 -0600

    Adds text to screen

--- BlackJack/main.cpp
+++ BlackJack/main.cpp
@@ -6,6 +6,13 @@
 
 #include <stdlib.h>
 #include <stdio.h>
+#include <sstream>
+#include <iostream>
+
+int score=0;
+int scoreDealer=0;
+int points=0;
+int pointsDealer=0;
 
 void init()
 {
@@ -17,13 +24,47 @@ void init()
     
 }
 
+std::string toString(int value) {
+    std::stringstream ss;
+    ss << value;
+    return ss.str();
+}
+
+void drawText(float x, float y, float size, std::string text, void* font) {
+    glMatrixMode(GL_MODELVIEW);
+    for (std::string::iterator i = text.begin(); i != text.end(); ++i)
+    {
+        glPushMatrix();
+        glScaled(size, size, 0);
+        glTranslatef(x, y, 0);
+        char c = *i;
+        glutStrokeCharacter(GLUT_STROKE_ROMAN, c);
+        glPopMatrix();
+        x+=70;
+    }
+}
+
 void dibuja()
 {
     glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
     
+    drawText(-400, 850, 0.5, "BLACKJACK", GLUT_BITMAP_9_BY_15);
+    
+    drawText(-1000, 850, 0.4, "SCORE " +toString(score), GLUT_BITMAP_9_BY_15);
+    drawText(-100, 850, 0.4, "SCORE DEALER " +toString(scoreDealer), GLUT_BITMAP_9_BY_15);
+    
+    drawText(-1200, 400, 0.4, "DEALER", GLUT_BITMAP_9_BY_15);
+    drawText(-1200, -300, 0.4, "PLAYER", GLUT_BITMAP_9_BY_15);
+    
+    drawText(500, 400, 0.3, "PTS DEALER " +toString(points), GLUT_BITMAP_9_BY_15);
+    drawText(500, -300, 0.3, "PTS PLAYER " +toString(pointsDealer), GLUT_BITMAP_9_BY_15);
+    
+    drawText(-1200, -900, 0.4, "D-DEAL H-HIT S-STAND ESC-SALIR", GLUT_BITMAP_9_BY_15);
+    drawText(-1200, -1200, 0.4, "AUTOR: ROBERTO MTZ A01190757 ", GLUT_BITMAP_9_BY_15);
+    
     glPushMatrix() ;
     glTranslated(0, 0, 0);
-    glutSolidCube(1);
+   
     glPopMatrix();
     
     glutSwapBuffers();
@@ -36,7 +77,7 @@ void reshape(int ancho, int alto)
     // Sistema de coordenadas
     glMatrixMode(GL_PROJECTION);
     glLoadIdentity();
-    glOrtho(-2, 2, -2, 2,1,12 ); //izq, der, abajo, arriba, cerca, lejos
+    glOrtho(-500, 500, -500, 500,1,12 ); //izq, der, abajo, arriba, cerca, lejos
     glMatrixMode(GL_MODELVIEW);
     glLoadIdentity();
     gluLookAt(0, 0, 10, 0, 0, 0, 0, 1, 0);
